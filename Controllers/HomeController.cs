756bb3e01d2ce033ef60b0e250799ab0572330f2
Kittisak Piyanath <kittisak.24@mail.kmutt.ac.th>
2017-06-06 11:09:24 +0700

    updatte function ReadJSON

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
+using WebApplication1.Models;
 
-namespace Appman_demo.Controllers
+namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
-
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            ViewBag.Message = "Your application description page.";
 
             return View();
         }
@@ -27,6 +27,13 @@ namespace Appman_demo.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public String Convert2()
+        {
+            string s1 = Data.ReadJson(@"C:\Users\mm\Documents\Visual Studio 2017\Projects\WebApplication1\WebApplication1\aaa.json");
+            
+            return s1;
         }
     }
 }
